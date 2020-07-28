16973ece3668e566374c7d0906b68e2c751e3bf1
tquangdo <trangia61@gmail.com>
2020-07-28 22:26:26 +0900

    add phpmyadmin for docker-> after run 'compose up' cmd, access 'http://localhost:8888/' for phpmyadmin

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=vocab_system',
+    'username' => 'root',
+    'password' => 'my-secret-pw',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
